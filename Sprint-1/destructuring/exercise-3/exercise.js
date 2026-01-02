6e4bc3555d2b2b43402fb978aedcad68df15e38f
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-03 00:09:44 +0100

    add program to print receipt

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,17 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+console.log("QTY     ITEM                TOTAL");
+let totalCost = 0;
+
+order.forEach(({ itemName, quantity, unitPricePence }) => {
+  const itemTotal = (quantity * unitPricePence) / 100;
+  totalCost += itemTotal;
+
+  console.log(
+    `${quantity}       ${itemName.padEnd(20)} ${itemTotal.toFixed(2)}`
+  );
+});
+
+console.log("\nTotal:", totalCost.toFixed(2));
