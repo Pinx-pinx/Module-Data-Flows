c78d6411a3854170fd6a68f5a77cfe93ef3ffdd1
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-04 15:28:44 +0100

    change if in submit function

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -28,20 +28,20 @@ const check = document.getElementById("check");
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
 function submit() {
-  if (
-    title.value == null ||
-    title.value == "" ||
-    pages.value == null ||
-    pages.value == ""
-  ) {
-    alert("Please fill all fields!");
-    return false;
-  } else {
+  if (!title.value || !author.value || !pages.value) {
+  alert("Please fill all fields!");
+  return false;
+}
    let book = new Book(title.value, author.value, pages.value, check.checked);
 myLibrary.push(book);
+title.value = "";
+author.value = "";
+pages.value = "";
+check.checked = false;
+
     render();
   }
-}
+
 
 function Book(title, author, pages, check) {
   this.title = title;
