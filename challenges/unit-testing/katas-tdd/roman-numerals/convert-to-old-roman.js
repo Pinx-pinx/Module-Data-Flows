442c7a0958ace0067ac9a444faaf336948ddcc6e
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-05 17:26:08 +0100

    add function for old roman numerals

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,3 +1,26 @@
-function convertToOldRoman(n) {}
+function convertToOldRoman(n) {
+  let result = "";
+
+
+  const romanNumerals = [
+    { value: 1000, symbol: "M" },
+    { value: 500, symbol: "D" },
+    { value: 100, symbol: "C" },
+    { value: 50, symbol: "L" },
+    { value: 10, symbol: "X" },
+    { value: 5, symbol: "V" },
+    { value: 1, symbol: "I" }
+  ];
+
+ 
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (n >= romanNumerals[i].value) {
+      result += romanNumerals[i].symbol;
+      n -= romanNumerals[i].value;
+    }
+  }
+
+  return result;
+}
 
 module.exports = convertToOldRoman;
