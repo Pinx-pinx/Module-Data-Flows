6339de9c7d2b9b71347fb16831d18a2b90fa21f3
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-05 17:25:25 +0100

    add function for newroman numerals

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,3 +1,31 @@
-function convertToNewRoman(n) {}
+function convertToNewRoman(n) {
+  let result = "";
+
+ 
+  const romanNumerals = [
+    { value: 1000, symbol: "M" },
+    { value: 900, symbol: "CM" },
+    { value: 500, symbol: "D" },
+    { value: 400, symbol: "CD" },
+    { value: 100, symbol: "C" },
+    { value: 90, symbol: "XC" },
+    { value: 50, symbol: "L" },
+    { value: 40, symbol: "XL" },
+    { value: 10, symbol: "X" },
+    { value: 9, symbol: "IX" },
+    { value: 5, symbol: "V" },
+    { value: 4, symbol: "IV" },
+    { value: 1, symbol: "I" }
+  ];
+
+  for (let i = 0; i < romanNumerals.length; i++) {
+    while (n >= romanNumerals[i].value) {
+      result += romanNumerals[i].symbol;
+      n -= romanNumerals[i].value;
+    }
+  }
+
+  return result;
+}
 
 module.exports = convertToNewRoman;
