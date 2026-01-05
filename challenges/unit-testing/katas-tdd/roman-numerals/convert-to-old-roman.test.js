7f5456f59f659783aaa3bc329a8eee55994de61d
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-05 17:26:21 +0100

    add test cases for old roman numeral

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
@@ -1,7 +1,26 @@
 let convertToOldRoman = require("./convert-to-old-roman");
 
 test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+  expect(convertToOldRoman(1)).toBe("I");
+});
+
+test("returns III if passed 3 as an argument", function () {
+  expect(convertToOldRoman(3)).toBe("III");
+});
+
+test("returns VII if passed 7 as an argument", function () {
+  expect(convertToOldRoman(7)).toBe("VII"); 
+});
+
+test("returns XV if passed 15 as an argument", function () {
+  expect(convertToOldRoman(15)).toBe("XV");
+});
+
+test("returns XVIII if passed 18 as an argument", function () {
+  expect(convertToOldRoman(18)).toBe("XVIII"); 
+});
+
+
+test("returns XXII if passed 22 as an argument", function () {
+  expect(convertToOldRoman(22)).toBe("XXII");
 });
