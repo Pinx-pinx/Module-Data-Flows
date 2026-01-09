9e8cbb4c205dcfd86928910b5b659dc91e6fb3d2
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-09 19:29:18 +0100

    implement test cases

--- challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
+++ challenges/unit-testing/writing-tests/largest-number/largest-number.test.js
@@ -1,9 +1,16 @@
 let getLargestNumber = require("./largest-number");
 
-test("returns largest number in array", function () {
+test("returns largest number in array and checks original array is unchanged", function () {
   // Arrange
+  const numbers = [3, 21, 88, 4, 36];
+  const expected = 88;
+  const originalArray = [...numbers];  
   // Act
+  const result = getLargestNumber(numbers);
+
   // Assert
+  expect(result).toEqual(expected);  
+  expect(numbers).toEqual(originalArray);  
 });
 
 // example
