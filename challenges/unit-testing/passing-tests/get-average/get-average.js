571e819681165dd93166e02566637af1c2beaa2b
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-09 19:29:03 +0100

    populate the function

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,18 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+
+  const numericValues = numbers.filter(item => typeof item === 'number');
+ 
+  if (numericValues.length === 0) return 0;
+
+  
+  const sum = numericValues.reduce((total, num) => total + num, 0);
+
+  return sum / numericValues.length;
+}
+
+
 
 module.exports = average;
