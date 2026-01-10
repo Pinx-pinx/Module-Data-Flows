8e5f0191774bfaedfb34bf9fa4387d2db59c480d
Pinx-pinx <pinkybenitez3364@gmail.com>
2026-01-10 15:45:03 +0100

    add function to fetch data for a city

--- challenges/challenge-weather-app/script.js
+++ challenges/challenge-weather-app/script.js
@@ -1,3 +1,19 @@
 
 const API_KEY_UNSPLASH = 'ultUyUIgKl_K25Y6i3Kjd7q39AloyhYVATc_0ReMf_8'; 
-const API_KEY_OPENWEATHER = '65e6f8a25588f270af49a494b6d45181'; // 
+const API_KEY_OPENWEATHER = '65e6f8a25588f270af49a494b6d45181'; 
+
+function getWeather(city) {
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY_OPENWEATHER}&units=metric`;
+
+  
+  fetch(weatherUrl)
+    .then(response => response.json()) 
+    .then(data => {
+      console.log(data); 
+      displayWeather(data); 
+      getImages(data.weather[0].description); 
+    })
+    .catch(error => {
+      console.error('Error fetching weather data:', error); 
+    });
+}
